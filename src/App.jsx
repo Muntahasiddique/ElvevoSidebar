2c69823685153a74d5397444c9919e498c5242a3
muntaha <muntahamirza890@gmail.com>
2025-08-17 00:22:17 +0500

    Added Dummy of sidebar

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='Main' >
+  <header>Sidebar</header>
+  <a href="/">Home</a>
+   <a href="/">About</a>
+    <a href="/">Contact</a>
+    </div>
+    </>
+  )
+}
+
+export default App
