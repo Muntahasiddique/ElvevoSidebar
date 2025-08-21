45038a279a92cb1ce3ab215898b3dce4f01357d7
muntaha <muntahamirza890@gmail.com>
2025-08-22 02:53:48 +0500

    Added sidebar with all icons styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 
 import './App.css'
 import Sidebar from './components/Sidebar'
-
-
+import Sidebardata from './components/Sidebardata'
+import HomeIcon from '@mui/icons-material/Home';
+import Grid3x3Icon from '@mui/icons-material/Grid3x3';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import TaskIcon from '@mui/icons-material/Task';
+import FlagIcon from '@mui/icons-material/Flag';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 function App() {
 
 
   return (
     <>
    <div className='flex' >
-< Sidebar />
+
+< Sidebar >
+<Sidebardata icon={<HomeIcon size={20} />} text="Home" alert />
+        <Sidebardata icon={< Grid3x3Icon size={20} />} text="Dashboard" active />
+        <Sidebardata icon={<AnalyticsIcon size={20} />} text="Analytics" />
+        <Sidebardata icon={<SupportAgentIcon size={20} />} text="Customers" alert />
+        <Sidebardata icon={< AccountTreeIcon size={20} />} text="Projects" />
+        <Sidebardata icon={<DocumentScannerIcon size={20} />} text="Documents" />
+        <Sidebardata icon={<CalendarMonthIcon size={20} />} text="Calendar" />
+        <Sidebardata icon={<TaskIcon size={20} />} text="Tasks" alert />
+        <Sidebardata icon={<FlagIcon  size={20} />} text="Reporting" />
+        <Sidebardata icon={<MessageIcon size={20} />} text="Messages" alert />
+        <Sidebardata icon={<NotificationsNoneIcon  size={20} />} text="Notifications" />
+</Sidebar>
+
    </div>
     </>
   )
--- src/components/Sidebardata.jsx
+++ src/components/Sidebardata.jsx
@@ -1,9 +1,21 @@
 
 
-function Sidebardata() {
+function Sidebardata({icon , text , active , alert}) {
   return (
     <div>
-      
+      <li className={` relative flex items-center py-3 px-4 my-1
+        font-medium rounded-md cursor-pointer
+        transition-all duration-200 ease-in-out text-gray-600 hover:bg-gray-50 hover:text-gray-900`} >
+        {icon}
+        <span className="ml-3 transition-all duration-200" >
+{text}
+        </span>
+        {alert && (<div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400`} >
+
+           </div>)
+          
+        }
+      </li>
     </div>
   )
 }
